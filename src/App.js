bbc6d5ef57f2f042b851f18e192623ce6f40bb7d
Tyler Briskie <tylerbriskie@gmail.com>
2019-10-14 16:27:37 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.newNumber = this.newNumber.bind(this);
+
+  }
+  state = {
+    randomInt: 42,
+  }
+  
+  newNumber(){
+    var newNumber = Math.floor(Math.random()*10000);
+    this.setState({randomInt: newNumber});
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <header id="App-header">
+          Testable Protractor App
+        </header>
+        <div id="random-int-container">
+          <h2>Random Number</h2>
+          <h3>{this.state.randomInt}</h3>
+          <button onClick={this.newNumber}>New Number</button>
+        </div>
+        <div id="card-container">
+        
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
